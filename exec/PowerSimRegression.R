8eaa702f33ead19d28e70bc6e642ef88f970d88c
Curtis Miller <cmiller@math.utah.edu>
2019-02-22 00:13:25 -0700

    Further work on simulation workhorse

--- exec/PowerSimRegression.R
+++ exec/PowerSimRegression.R
@@ -31,7 +31,7 @@ check_envir_has_objects <- CPAT:::check_envir_has_objects
 
 main <- function(SIMINPUT, CONTEXTINPUT, output = NULL, seed = 20190219,
                  seedless = FALSE, replications = 5000, cores = NULL,
-                 verbose = FALSE, help = FALSE) {
+                 verbose = FALSE, notsafe = FALSE, help = FALSE) {
   # This function will be executed when the script is called from the command
   # line; the help parameter does nothing, but is needed for do.call() to work
 
@@ -52,19 +52,98 @@ main <- function(SIMINPUT, CONTEXTINPUT, output = NULL, seed = 20190219,
   load(SIMINPUT, envir = simulation_tools)
   load(CONTEXTINPUT, envir = context_tools)
   # TODO: curtis: CHECK THAT NEEDED OBJECTS ARE IN PLACE -- Tue 19 Feb 2019 05:53:30 PM MST
-  simulation_tools_expected_objects <- c()
-  context_tools_expected_objects <- c("n_values")
+  simulation_tools_expected_objects <- c("df_generator", "eps_generator")
+  context_tools_expected_objects <- c("n_values", "kstar_functions",
+                                      "struc_models", "stat_functions")
   check_envir_has_objects(simulation_tools_expected_objects,
                           envir = simulation_tools, blame_string = SIMINPUT)
   check_envir_has_objects(context_tools_expected_objects, envir = context_tools,
                           blame_string = CONTEXTINPUT)
   # It should be safe now to pull what we want from the files we loaded
-
+  # But always check assumptions
   n_values <- context_tools$n_values
   stop_with_message(is.integer(n_values) & all(n_values > 0),
                     "Invalid n_values from" %s% CONTEXTINPUT %s0% "; must" %s%
                     "be positive integers")
+  kstar_functions <- context_tools$kstar_functions
+  stop_with_message(is.vector(kstar_functions) &
+                    is.function(kstar_functions[1] &
+                      !any(is.null(names(kstar_functions)))),
+                    "Invalid kstar_functions from" %s% CONTEXTINPUT %s0% ";" %s%
+                    "must be named vector of functions")
+  struc_models <- context_tools$struc_models
+  stop_with_message(is.list(struc_models) &
+                    !any(is.null(names(struc_models))) &
+                    all(sapply(struc_models, is.matrix)) &
+                    all(sapply(struc_models, function(m) {ncol(m) == 2})) &
+                    is.numeric(as.vector(sapply(struc_models, as.vector))),
+                    "Invalid struc_models from" %s% CONTEXTINPUT %s0% "; it" %s%
+                    "must be a named list of numeric matrices that have two" %s%
+                    "columns")
+  stat_functions <- context_tools$stat_functions
+  stop_with_message(is.vector(stat_functions) & is.function(stat_function[1]) &
+                    !any(is.null(names(stat_functions))), "Invalid" %s%
+                    "stat_functions from" %s% CONTEXTINPUT %s0% "; must be" %s%
+                    "a named vector of functions")
   
+  df_generator <- simulation_tools$df_generator
+  stop_with_message(is.function(df_generator) & all(c("n", "beta", "eps") %in%
+                    names(formals(df_generator))), "Invalid df_generator" %s%
+                    "from" %s% SIMINPUT %s0% "; must be a function with" %s%
+                    "arguments n, beta, and eps")
+  eps_generator <- simulation_tools$eps_generator
+  stop_with_message(is.function(df_generator) & "n" %in%
+                    names(formals(eps_generator)), "Invalid eps_generator" %s%
+                    "from" %s% SIMINPUT %s0% "; must be a function with" %s%
+                    "argument n")
+
+  # TODO: curtis: CREATE LIST FOR STORING DATA -- Fri 22 Feb 2019 12:07:40 AM MST
+  # TODO: curtis: CREATE SAVE FILE NAME IF NULL -- Fri 22 Feb 2019 12:12:44 AM MST
+  for (n in n_values) {
+    # TODO: curtis: CREATE INTERMEDIATE LISTS -- Fri 22 Feb 2019 12:08:17 AM MST
+    nname <- "n" %s0% n
+    for (kstar_name in names(kstar_functions)) {
+      kstar <- kstar_functions[kstar_name]
+      k <- floor(kstar(n))
+      stop_with_message(is.numeric(k) & k >= 1 & k <= n, "The function in" %s%
+                        "kstar_functions called" %s% kstar_name %s% "did" %s%
+                        "not return a number between 1 and" %s% n %s% "when" %s%
+                        "expected; this should happen when" %s% kstar_name %s0%
+                        "(n) is called")
+      for (stat_name in names(stat_functions)) {
+        stat <- stat_functions[stat_name]
+        stop_with_message(all(c("data", "formula") %in% names(formals(stat))),
+                          "The function in stat_functions called" %s% 
+                          stat_name %s% "does not have all of the" %s%
+                          "expected arguments (data, formula)")
+        for (r_name in names(struc_models)) {
+          r <- struc_models[r_name]
+          beta1 <- r[, 1]
+          beta2 <- r[, 2]
+          d <- nrow(r)
+
+          simulation <- foreach(throwaway = 1:replications,
+                                .combine = c) %dorng% {
+            eps <- eps_generator(n = n)
+            df <- rbind(df_generator(n = k, beta = beta1, eps = eps[1:k]),
+                        df_generator(n = (n - k), beta = beta2,
+                                     eps = eps[max((k + 1), n):n]))
+            stop_with_message("y" %in% names(df), "y must be one of the" %s%
+                              "columns generated by the data frame returned" %s%
+                              "by df_generator(); it is the dependent variable")
+            names(df[which(names(df) != "y")]) <- paste0("X", 1:d)
+            f <- "y" %s% "~" %s% paste0("X", 1:d, collapse = " + ")
+            f <- as.formula(f)
+            
+            stat(formula = f, data = df)
+          }
+          # TODO: curtis: SAVE RESULTS IF NOTSAFE IS FALSE -- Fri 22 Feb 2019 12:08:57 AM MST
+          # TODO: curtis: IF VERBOSE REPORT PROGRESS -- Fri 22 Feb 2019 12:08:57 AM MST
+        }
+      }
+    }
+  }
+  # TODO: curtis: SAVE RESULTS FOR LAST TIME -- Fri 22 Feb 2019 12:09:20 AM MST
 }
 
 ################################################################################
@@ -100,7 +179,11 @@ if (sys.nframe() == 0) {
                              "1 to effectively disable parallelization, or" %s%
                              "leave unset to use default (all but one core)"),
           make_option(c("--verbose", "-v"), action = "store_true",
-                      default = FALSE, help = "Report progress on screen")
+                      default = FALSE, help = "Report progress on screen"),
+          make_option(c("--notsafe", "-a"), action = "store_true",
+                      default = FALSE,
+                      help = "Don't save frequently; only save after all" %s%
+                             "simulations are done")
         )
       ))
 
