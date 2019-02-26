d729de7bf7d10bb34302616ad2d8b006c29a966b
Curtis Miller <cmiller@math.utah.edu>
2019-02-25 17:15:25 -0700

    Started work on plotter

---
+++ exec/Plotter.R
@@ -0,0 +1,45 @@
+#!/usr/bin/Rscript
+################################################################################
+# Plotter.R
+################################################################################
+# 2019-02-25
+# Curtis Miller
+################################################################################
+# This is a one-line description of the file.
+################################################################################
+
+# argparser: A package for handling command line arguments
+if (!suppressPackageStartupMessages(require("argparser"))) {
+  install.packages("argparser")
+  require("argparser")
+}
+
+################################################################################
+# EXECUTABLE SCRIPT MAIN FUNCTIONALITY
+################################################################################
+
+main <- function(foo, bar = 0, baz = FALSE, help = FALSE, opts = NA) {
+  # This function will be executed when the script is called from the command
+  # line; the help and opts parameters does nothing, but are needed for
+  # do.call() to work
+
+  print("Hello!")
+}
+
+################################################################################
+# INTERFACE DEFINITION AND COMMAND LINE IMPLEMENTATION
+################################################################################
+
+if (sys.nframe() == 0) {
+  p <- arg_parser("This is a template for executable R scripts.")
+  p <- add_argument(p, "foo", type = "character", nargs = 1,
+                    help = "A positional command-line argument")
+  p <- add_argument(p, "--bar", type = "integer", default = 0,
+                    nargs = 1, help = "A command-line option")
+  p <- add_argument(p, "--baz", flag = TRUE, help = "A command-line flag")
+
+  cl_args <- parse_args(p)
+
+  do.call(main, cl_args[2:length(cl_args)])
+}
+
